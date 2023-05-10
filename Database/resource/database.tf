316b6f6241b9aa2817d6905bf3faae51108a8a99
Renan <renansaldlinhares@gmail.com>
2023-05-10 18:50:50 +1200

    change source

--- Database/resource/database.tf
+++ Database/resource/database.tf
@@ -19,7 +19,7 @@ resource "random_string" "password" {
 }
 
 module "mysql_server" {
-    source                        = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//Azure/mysqlserver"
+    source                        = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform/Azure/mysqlserver"
     env                           = local.env
     responsable                   = local.responsable
     name                          = "application"
