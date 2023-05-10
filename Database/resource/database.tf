0e69510aa5bbca95ce44194bb30b8e37634b253a
Renan <renansaldlinhares@gmail.com>
2023-05-10 17:17:34 +1200

    update resource group and database

---
+++ Database/resource/database.tf
@@ -0,0 +1,46 @@
+variable "CONTAINER_NAME" {}
+variable "RESOURCE_GROUP_NAME" {}
+variable "STORAGE_ACCOUNT_NAME" {}
+variable "KEY" {}
+
+locals {
+  env         = "dev"
+  responsable = "Renan Saldanha"
+}
+
+resource "random_string" "admin" {
+  length  = 8
+  special = false
+}
+
+resource "random_string" "password" {
+  length  = 16
+  special = true
+}
+
+module "mysql_server" {
+    source                        = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//Azure/mysqlServer"
+    env                           = local.env
+    responsable                   = local.responsable
+    name                          = "application"
+    location                      = module.rg.region
+    resource_group_name           = module.rg.rg_name
+    sku_name                      = "B_Gen4_1"
+    version                       = "8.0"
+    administration_login          = random_string.admin.result
+    administration_login_password = random_string.password.result
+    public_network_access_enabled = true
+    instance = 1
+    BACKEND_CONTAINER_NAME        = "${var.CONTAINER_NAME}"
+    BACKEND_RESOURCE_GROUP_NAME   = "${var.RESOURCE_GROUP_NAME}"
+    BACKEND_STORAGE_ACCOUNT_NAME  = "${var.STORAGE_ACCOUNT_NAME}"
+    BACKEND_KEY                   = "${var.KEY}"
+}
+
+module "mysql_db" {
+    source      = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//Azure/mysql"
+    env         = local.env
+    responsable = local.responsable
+    name        = "db_application"
+    server_id   = module.mysql_server.server_id
+}
--- ResourceGroup/resourceGroup.tf
+++ ResourceGroup/resourceGroup.tf
@@ -14,12 +14,12 @@ locals {
 module "rg" {
     source               = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//Azure/rg-module"
 
-    env                  = local.env
-    responsable          = local.responsable
-    region               = local.region
-    rg_name              = local.rg_name
-    CONTAINER_NAME       = "${var.CONTAINER_NAME}"
-    RESOURCE_GROUP_NAME  = "${var.RESOURCE_GROUP_NAME}"
-    STORAGE_ACCOUNT_NAME = "${var.STORAGE_ACCOUNT_NAME}"
-    KEY                  = "${var.KEY}"
+    env                          = local.env
+    responsable                  = local.responsable
+    region                       = local.region
+    rg_name                      = local.rg_name
+    BACKEND_CONTAINER_NAME       = "${var.CONTAINER_NAME}"
+    BACKEND_RESOURCE_GROUP_NAME  = "${var.RESOURCE_GROUP_NAME}"
+    BACKEND_STORAGE_ACCOUNT_NAME = "${var.STORAGE_ACCOUNT_NAME}"
+    BACKEND_KEY                  = "${var.KEY}"
 }
