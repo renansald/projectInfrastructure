0bbecf6e8180a339abe1cecdfe2932ca62a9d5e7
Renan <renansaldlinhares@gmail.com>
2023-05-11 17:12:02 +1200

    update pipeline

--- Database/resource/database.tf
+++ Database/resource/database.tf
@@ -19,7 +19,7 @@ resource "random_string" "password" {
 }
 
 module "mysql_server" {
-    source                        = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//mysqlserver/" 
+    source                        = "git::ssh://git@ssh.dev.azure.com/v3/renanlinhares01/terraform/terraform//mysqlserver" 
     env                           = local.env
     responsable                   = local.responsable
     name                          = "application"
