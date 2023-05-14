58a308d3bc7753a2fa5c92c554bd7778ea420f43
Renan <renansaldlinhares@gmail.com>
2023-05-14 15:11:42 +1200

    test

--- terraform/version.tf
+++ terraform/version.tf
@@ -4,18 +4,20 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
-    features {
-      provider_locking {
-        enable = true
-      }
-    }
   }
+  
   backend "azurerm" {
     resource_group_name   = var.BACKEND_RESOURCE_GROUP_NAME
     storage_account_name  = var.BACKEND_STORAGE_ACCOUNT_NAME
     container_name        = var.BACKEND_CONTAINER_NAME
     key                   = var.BACKEND_KEY
     use_azuread_auth      = true
+
+    features {
+      provider_locking {
+        enable = true
+      }
+    }
   }
   
   required_version = ">= 1.0, <2.0"
